80b84bac808682c905def4a19d2dfeb7d64d5015
Kimmy X <qaqaqaq100000@outlook.com>
2026-02-28 21:37:12 +0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,99 @@
+#include <bits/stdc++.h>
+using namespace std;
+int check(tuple<char, int, int> piece_a, tuple<char, int, int> piece_b) {
+	int total = 0;
+	auto [type_a, a_x, a_y] = piece_a;
+	auto [type_b, b_x, b_y] = piece_b;
+	if(type_a == 'Q') {
+		if(abs(a_x - b_x) == abs(a_y - b_y)) total++;
+		else if(a_x == b_x || a_y == b_y) total++;
+	}
+	if(type_a == 'R') {
+		if(a_x == b_x || a_y == b_y) total++;
+	}
+	if(type_a == 'B') {
+		if(abs(a_x - b_x) == abs(a_y - b_y)) total++;
+	}
+	return total;
+
+}
+int solve(int N, int M, int K, vector<tuple<char, int, int>> P) {
+    int total = 0;
+	// N: Number of rows of the board
+    // M: Number of columns of the board
+    // K: Number of pieces
+    // P: list of tuples. Each tuple contains (piece type, x position, y position)
+	vector<tuple<char, int, int>> hori (P);
+	sort(hori.begin(), hori.end(), [](auto a, auto b){
+		return get<1>(a) < get<1>(b);
+	});
+	
+	for(int i = 0; i < hori.size(); i++)
+	{
+		int a = i, b = i + 1;
+		auto [atype, ax, ay] = hori[a]; auto [btype, bx, by] = hori[b];
+		if(ax == bx){
+			total += check(hori[a], hori[b]); total += check(hori[b], hori[a]);
+		}
+	}
+//------
+	vector<tuple<char, int, int>> verti (P);
+	sort(verti.begin(), verti.end(), [](auto a, auto b){
+		return get<2>(a) < get<2>(b);
+	});
+
+	for(int i = 0; i < verti.size(); i++)
+	{
+		int a = i, b = i + 1;
+		auto [atype, ax, ay] = verti[a]; auto [btype, bx, by] = verti[b];
+		if(ay == by){
+			total += check(verti[a], verti[b]); total += check(verti[b], verti[a]);
+		}
+	}
+//------
+	vector<tuple<char, int, int>> LtoR_diag (P);
+	sort(LtoR_diag.begin(), LtoR_diag.end(), [](auto a, auto b){
+		return get<1>(a) + get<2>(a) < get<1>(b) + get<2>(b);// x + y sort
+	});
+	for(int i = 0; i < LtoR_diag.size(); i++)
+	{
+		int a = i, b = i + 1;
+		auto [atype, ax, ay] = LtoR_diag[a]; auto [btype, bx, by] = LtoR_diag[b];
+		if(ax + ay == bx + by){
+			total += check(LtoR_diag[a], LtoR_diag[b]); total += check(LtoR_diag[b], LtoR_diag[a]);
+		}
+	}
+//------
+	vector<tuple<char, int, int>> RtoL_diag (P);
+	sort(RtoL_diag.begin(), RtoL_diag.end(), [&N](auto a, auto b){
+		return N + 1 - get<1>(a) + get<2>(a) < N + 1 - get<1>(b) + get<2>(b) ; // N + 1 - x + y  sort
+	});
+	for(int i = 0; i < RtoL_diag.size(); i++)
+	{
+		int a = i, b = i + 1;
+		auto [atype, ax, ay] = RtoL_diag[a]; auto [btype, bx, by] = RtoL_diag[b];
+		if(N + 1 - ax + ay == N + 1 - bx + by){
+			total += check(RtoL_diag[a], RtoL_diag[b]); total += check(RtoL_diag[b], RtoL_diag[a]);
+		}
+	}
+
+	
+	
+	return total;
+}
+
+int main() {
+	ios::sync_with_stdio(false); 
+	cin.tie(NULL); cout.tie(NULL);
+	int t; cin >> t;
+	for (int i = 0; i < t; i++) {
+		int n, m; cin >> n >> m;
+		int k; cin >> k;
+		vector<tuple<char, int, int>> pieces;
+		for (int i = 0; i < k; i++) {
+			char piece; int x, y; cin >> piece >> x >> y;
+            pieces.emplace_back(piece, x, y);
+		}
+	    cout << solve(n, m, k, pieces) << "\n";
+	}
+}
